3624fd45d76fa3c25c086a5169b7e9e5224eb95f
Saheer Anas <saheer09anas@gmail.com>
2019-07-02 11:47:08 +0530

    Removed stray code, updated sample data

--- app/actions/index.js
+++ app/actions/index.js
@@ -1,15 +1,11 @@
 export const DATA_AVAILABLE = "DATA_AVAILABLE";
-
 //Import the sample data
-import Data from "../instructions.json";
+import Data from "../sampledata.json";
 
 export function getData() {
   return dispatch => {
-    //Make API Call
-    //For this example, I will be using the sample data in the json file
-    //delay the retrieval [Sample reasons only]
     setTimeout(() => {
-      const data = Data.instructions;
+      const data = Data.faq;
       dispatch({ type: DATA_AVAILABLE, data: data });
     }, 2000);
   };
