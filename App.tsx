50633f7c1ec1f107ae45a96752d352ff0ec7c8bf
Saheer Anas <saheer09anas@gmail.com>
2022-09-20 23:56:35 +0530

    Remove unnecessary code

--- App.tsx
+++ App.tsx
@@ -8,11 +8,8 @@ import {NoInternetToast} from './app/components/NoInternet';
 // Navigation
 import RootNavigation from './app/routes/RootNavigation';
 
-import {API_URL} from 'react-native-dotenv';
-
 export default class App extends Component {
   render() {
-    console.log(API_URL);
     return (
       <Provider store={store}>
         <ThemeProvider>
