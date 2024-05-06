6e981f6e267bcbd12aa30f45e741c0d89d1ec8ab
Saheer Anas <saheer09anas@gmail.com>
2024-05-06 16:04:01 +0530

    Fix component testing for RootNAvigation

--- e2e/starter.test.js
+++ e2e/starter.test.js
@@ -7,17 +7,16 @@ describe('Example', () => {
     await device.reloadReactNative();
   });
 
-  it('should have welcome screen', async () => {
-    await expect(element(by.id('welcome'))).toBeVisible();
+  it('should have a To Do screen as home screen', async () => {
+    await expect(element(by.text('To Do'))).toBeVisible();
   });
 
-  it('should show hello screen after tap', async () => {
-    await element(by.id('hello_button')).tap();
-    await expect(element(by.text('Hello!!!'))).toBeVisible();
+  it('should show network screen after tap', async () => {
+    await element(by.id('BottomTab.Network')).tap();
+    await expect(element(by.text('NetworkExample'))).toBeVisible();
   });
 
-  it('should show world screen after tap', async () => {
-    await element(by.id('world_button')).tap();
-    await expect(element(by.text('World!!!'))).toBeVisible();
+  afterAll(async () => {
+    await device.terminateApp();
   });
 });
